5f69c8c11ce671dd37d0a215da9e684ca5587bc4
Aluno <Aluno@172.16.2.53>
2026-08-12 11:41:32 -0300

    bla2

--- src/tinderquackk/Main.java
+++ src/tinderquackk/Main.java
@@ -53,32 +53,98 @@ public class Main {
 	}
 
 	private static void cadastrarPato(Scanner entrada) {
-	
-	}
+		System.out.println("Qual tipo de Pato? 1 - Doméstico | 2 - Selvagem");
+		int tipo = entrada.nextInt();
+		entrada.nextLine();
 		
-	private static void listarPatos() {
+		Pato novoPato;
+		if(tipo == 1) {
+			novoPato = new PatoDomestico();
+		}else {
+			novoPato = new PatoSelvagem();
+		}
+		
+		novoPato.cadastrar();
+		listaPatos.add(novoPato);
+		System.out.println("Pato cadastrado com sucesso! ID: " + novoPato.getIdPato());
 	}
+	
+	private static void listarPatos() {
+		if (listaPatos.isEmpty()) {
+			System.out.println("Nenhum pato cadastrado.");
+		}else {
+			System.out.println("\n --- Lista de Patos ---");
+			for(Pato p : listaPatos) {
+				System.out.println("ID: " + p.getIdPato() + " | Nome: " 
+			+ p.getNome() + " | Email: " + p.getEmail());
+				if(p instanceof PatoDomestico) {
+					System.out.println(" (Doméstico -  Raça: " + ((PatoDomestico) p)
+							.getRegiaoOrigem() + ")");
+				}
+			}
+		}
+	}
+	
 	private static void buscarPato(Scanner entrada) {
+		System.out.println("Digite o ID do Pato: ");
+		int id = entrada.nextInt();
+		Pato encontrado = null;
+		
+		for (Pato p: listaPatos) {
+			if(p.getIdPato() == id) {
+				encontrado = p;
+				break;
+			}
+		}
+		
+		if (encontrado != null) {
+			System.out.println("Pato encontrado: " + encontrado.getNome() + " (" 
+		+ encontrado.getEmail() + ")");
+		}else {
+			System.out.println("Pato não encontrado.");
+		}
 	}
 	private static void atualizarPato(Scanner entrada) {
+		System.out.println("Digite o ID do Pato a ser atualizado:");
+		int id = entrada.nextInt();
+		entrada.nextLine();
+		
+		Pato encontrado = null;
+		for (Pato p: listaPatos) {
+			if (p.getIdPato() == id) {
+				encontrado = p;
+				break;
+			}
+		}
+		if (encontrado != null) {
+			encontrado.editarDados();
+			System.out.println("Dados atualizados com sucesso!");
+		}else {
+			System.out.println("Pato não encontrado.");
+		}
 	}
 	private static void deletarPato(Scanner entrada) {
-	}
+		System.out.println("Digite o ID do pato a ser deletado: ");
+		int id = entrada.nextInt();
+		
+		Pato encontrado = null;
+		for (Pato p: listaPatos) {
+			if(p. getIdPato() == id) {
+				encontrado = p;
+				break;
+			}
+		}
+		if (encontrado != null) {
+		listaPatos.remove(encontrado);
+		System.out.println("Pato removido com sucesso!");
+		}else {
+			System.out.println("pato não encontrado.");
+			}
+		}
 	private static void criarPerfil(Scanner entrada) {
+		Perfil perfil = new Perfil();
+		perfil.cadastrar();
+		listaPerfis.add(perfil);
+		System.out.println("Perfil craido e vinculado ao sistema!");
+	  }	
 	}
-	
-		
-		
-		
-		
-		
-	}
-
-}
-
-
-
-
-
-
-
